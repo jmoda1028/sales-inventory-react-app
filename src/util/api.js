aeed90ae0e3fc739413e2044175d2cf9ba6eae37
jun <jmoda1028@gmail.com>
2023-04-19 15:56:11 +0800

    change api endpoint

--- src/util/api.js
+++ src/util/api.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 export default axios.create({
   // withCredentials: true,
   // baseURL: "https://sales-and-inventory-api.herokuapp.com/api/",
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://sales-and-inventory-api-new.vercel.app/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
   headers: {
     'Content-Type': 'application/json',
     'Authorization' : `Bearer ${localStorage.getItem('token')}`
